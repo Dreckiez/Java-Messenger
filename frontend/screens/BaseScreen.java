0416597b649b25c72c847166884c5b6dbe83ab56
Dreckiez <143894214+Dreckiez@users.noreply.github.com>
2025-11-12 05:48:44 +0700

    Login & Register Screen

---
+++ frontend/screens/BaseScreen.java
@@ -0,0 +1,37 @@
+package screens;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.CardLayout;
+
+public class BaseScreen extends JFrame{
+
+    private CardLayout layout;
+    private JPanel panel;
+
+    public BaseScreen(){
+        setTitle("Chat Sech with Loli");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(1280, 720);
+        
+        layout = new CardLayout();
+        panel = new JPanel(layout);
+
+        add(panel);
+        
+        ImageIcon image = new ImageIcon(getClass().getClassLoader().getResource("assets/wolf-howling.jpg"));
+        setIconImage(image.getImage());
+
+        setVisible(true);
+    }
+
+    public void addPanel(JPanel p, String name){
+        panel.add(p, name);
+    }
+
+    public void showPanel(String name){
+        layout.show(panel, name);
+    }
+}
