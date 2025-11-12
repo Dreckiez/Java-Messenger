6ec37a275f38a44c6d5f3746b1f5604e1896b2a5
Dehmin <minhnguyen061104@gmail.com>
2025-11-12 10:42:23 +0700

    All except chat feature

---
+++ backend/src/main/java/com/example/spring_security/dto/request/ManageUserRequest.java
@@ -0,0 +1,35 @@
+package com.example.spring_security.dto.request;
+
+import com.example.spring_security.entities.Enum.Gender;
+import com.example.spring_security.entities.Enum.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserRequest {
+    @NotBlank(message = "This field must not be blank.")
+    String username;
+    String firstName;
+    String lastName;
+    @NotBlank(message = "This field must not be blank.")
+    @Email(message = "This field must be a valid email.")
+    String email;
+    @NotBlank(message = "This field must not be blank.")
+    String password;
+    Gender gender;
+    Role role;
+    String address;
+    LocalDate birthDay;
+    boolean isActive;
+    boolean isAccepted;
+    boolean isOnline;
+    LocalDateTime joinedAt;
+    LocalDateTime updatedAt;
+}
---
+++ backend/src/main/java/com/example/spring_security/entities/Report.java
@@ -0,0 +1,4 @@
+package com.example.spring_security.entities;
+
+public class Report {
+}
