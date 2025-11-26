4683d20abea71b2c9475b7d6ac00a404a59afda1
Dehmin <minhnguyen061104@gmail.com>
2025-11-26 14:19:15 +0700

    modify: report

--- src/main/java/com/example/spring_security/Main.java
+++ src/main/java/com/example/spring_security/Main.java
@@ -1,17 +1,10 @@
 package com.example.spring_security;
-
-import com.example.spring_security.entities.Enum.Gender;
-import com.example.spring_security.entities.Enum.Role;
-import com.example.spring_security.entities.User;
-import com.example.spring_security.repository.UserRepository;
 import com.example.spring_security.services.third.InitializationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
--- src/main/java/com/example/spring_security/entities/Enum/ReportStatus.java
+++ src/main/java/com/example/spring_security/entities/Enum/ReportStatus.java
@@ -1,4 +1,5 @@
 package com.example.spring_security.entities.Enum;
 
 public enum ReportStatus {
+    PENDING, PROCESSED
 }
